3802d1dc602f00e3e9b287700ec331405ca51e64
Gustavo Villalta <gvillalta@users.noreply.github.com>
2021-02-15 14:49:52 -0300

    5.Hello, printf

---
+++ low-level/0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: exit status.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
