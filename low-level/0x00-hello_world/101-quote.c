7343cdd984a23d4495caa17f6d3be5da52253dd2
Gustavo Villalta <gvillalta@users.noreply.github.com>
2021-02-15 16:53:53 -0300

    fix

--- low-level/0x00-hello_world/101-quote.c
+++ low-level/0x00-hello_world/101-quote.c
@@ -3,25 +3,32 @@
 #include <unistd.h>
 
 /**
-	* main - Main function
-	*
-	* Return: exit status.
-	*/
-void write_str( char *string, int n )
+ * write_str - Replacement for printf, puts, etc
+ *
+ * Return: exit status.
+ */
+void write_str(char *string, int n)
 {
 	int len = strlen(string);
+
 	n = (n > 0) ? n : 1;
 
-	if((write(n, string, len)) != len)
-    {
-        write(2, "Error occurred", 16);
-        exit(2);
+	if ((write(n, string, len)) != len)
+	{
+		write(2, "Error occurred", 16);
+		exit(2);
 	}
 }
 
+/**
+ * main - Main function
+ *
+ * Return: exit status.
+ */
 int main(void)
 {
-    char phrase[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char phrase[] =
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write_str(phrase, 0);
 	return (1);
