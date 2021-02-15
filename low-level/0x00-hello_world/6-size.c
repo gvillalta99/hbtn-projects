3751ae04a79681b704822e62c6204b8b165f11c2
Gustavo Villalta <gvillalta@users.noreply.github.com>
2021-02-15 16:22:59 -0300

    Fix

--- low-level/0x00-hello_world/6-size.c
+++ low-level/0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Main function
- *
- * Return: exit status.
- */
+	* main - Main function
+	*
+	* Return: exit status.
+	*/
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)", sizeof(char));
@@ -12,6 +12,6 @@ int main(void)
 	printf("\nSize of a long int: %lu byte(s)", sizeof(long int));
 	printf("\nSize of a long long int: %lu byte(s)", sizeof(long long int));
 	printf("\nSize of a float: %lu byte(s)", sizeof(float));
-    printf("\n");
+	printf("\n");
 	return (0);
 }
