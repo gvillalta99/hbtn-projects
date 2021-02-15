23c48f6911a441d3ffdb05d2c392135475cd5794
Gustavo Villalta <gvillalta@users.noreply.github.com>
2021-02-15 16:50:13 -0300

    9. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ low-level/0x00-hello_world/101-quote.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+	* main - Main function
+	*
+	* Return: exit status.
+	*/
+void write_str( char *string, int n )
+{
+	int len = strlen(string);
+	n = (n > 0) ? n : 1;
+
+	if((write(n, string, len)) != len)
+    {
+        write(2, "Error occurred", 16);
+        exit(2);
+	}
+}
+
+int main(void)
+{
+    char phrase[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write_str(phrase, 0);
+	return (1);
+}
+
