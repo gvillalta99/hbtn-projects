adb1cc951d2b0d72806fbb3bef042c3e8532e02e
Gustavo Villalta <gvillalta@users.noreply.github.com>
2021-02-15 18:49:25 -0300

    1. The last digit

---
+++ low-level/0x01-c-variables-if-else-while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Main function
+ *
+ * Return: exit status.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+    int last_digit = n % 10;
+    if (last_digit < 0)
+        last_digit = - last_digit;
+
+    printf("Last digit of %d is %d and is ", n, last_digit);
+
+    if (last_digit == 0)
+        printf("0");
+    else if (last_digit < 6)
+        printf("less than 6 and not 0");
+    else
+        printf("greater than 5");
+
+
+    printf("\n");
+
+	return (0);
+}
