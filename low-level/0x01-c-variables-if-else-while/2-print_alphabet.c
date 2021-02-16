c9539bb3173689ede553886a07d6fd9dbb5332f6
Gustavo Villalta <gvillalta@users.noreply.github.com>
2021-02-16 11:52:04 -0300

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ low-level/0x01-c-variables-if-else-while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Main function
+ *
+ * Return: exit status.
+ */
+int main(void)
+{
+
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
