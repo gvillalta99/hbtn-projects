1b2ac790eddda102b8d9e2f8a9bef4d0d9e8c96b
Gustavo Villalta <gvillalta@users.noreply.github.com>
2021-02-16 12:03:57 -0300

    4. When I was having that alphabet soup, I never thought that it would pay off

---
+++ low-level/0x01-c-variables-if-else-while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Main function
+ *
+ * Return: exit status.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	    if (c == 'q' || c == 'e')
+		{
+			continue;
+		}
+
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
