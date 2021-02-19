fda5376de3fe4424e48e6b0b0d043362270abeb3
Gustavo Villalta <gvillalta@users.noreply.github.com>
2021-02-19 12:09:54 -0300

    fix

--- low-level/0x01-c-variables-if-else-while/100-print_comb3.c
+++ low-level/0x01-c-variables-if-else-while/100-print_comb3.c
@@ -10,14 +10,14 @@
  */
 int main(void)
 {
-	int d, u;
+	int d, u, i;
 
 	bool d_cache[10] = {false,false,false,false,false,false,false,false,false,false};
 	bool u_cache[10];
 
 	for (d = 0; d <= 9; d++)
 	{
-		for(int i = 0; i < 10; i++)
+		for(i = 0; i < 10; i++)
 			u_cache[i] = false;
 
 		for (u = 0; u <= 9 ; u++)
